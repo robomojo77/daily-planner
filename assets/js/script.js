ead6569cfe12ac9bb7c9b366e63b2590e25aff1a
Chris Bomm <wordsciencechris@gmail.com>
2021-01-17 13:14:17 -0600

    added unique ids to html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+// Global Time-Slot Array
+
 var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// Save Functions
+
+function saveTimeslot() {
+
+}
+
+function retrieveTimeSlots () {
+
+}
+
+retrieveTimeSlots();
+
+// Time color-coding
+
 var currentTime = moment().hour();
 for (var i = 0; i < businessHours.length; i++) {
     if (currentTime == businessHours[i]) {
@@ -9,3 +26,8 @@ for (var i = 0; i < businessHours.length; i++) {
         $("#work-hour_" + i).addClass("future");
     }
 }
+
+// Save Button Listener
+
+$
+
