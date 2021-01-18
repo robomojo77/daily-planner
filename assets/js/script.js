f26320084153e8cd40c42965bc0051384a2cdbb0
Chris Bomm <wordsciencechris@gmail.com>
2021-01-17 18:38:24 -0600

    set up local storage save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
 // Global Time-Slot Array
 
-var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var businessHours = [
+    {
+        id: 9,
+        task: "Available",
+    },
+    {
+        id: 10,
+        task: "Available",
+    },
+    {
+        id: 11,
+        task: "Available",
+    },
+    {
+        id: 12,
+        task: "Available",
+    },
+    {
+        id: 13,
+        task: "Available",
+    },
+    {
+        id: 14,
+        task: "Available",
+    },
+    {
+        id: 15,
+        task: "Available",
+    },
+    {
+        id: 16,
+        task: "Available",
+    },
+    {
+        id: 17,
+        task: "Available",
+    },
+];
 
 // Save Functions
 
@@ -8,8 +45,14 @@ function saveTimeslot() {
 
 }
 
-function retrieveTimeSlots () {
-
+function retrieveTimeSlots() {
+    for (var i = 0; i < businessHours.length; i++) {
+        var testString = localStorage.getItem("savedTask_" + i)
+        if (testString != undefined) {
+            businessHours[i].task = testString;
+            document.getElementById("timeSlot_" + i).innerHTML = testString;
+        }
+    }
 }
 
 retrieveTimeSlots();
@@ -18,9 +61,9 @@ retrieveTimeSlots();
 
 var currentTime = moment().hour();
 for (var i = 0; i < businessHours.length; i++) {
-    if (currentTime == businessHours[i]) {
+    if (currentTime == businessHours[i].id) {
         $("#work-hour_" + i).addClass("present");
-    } else if (currentTime > businessHours[i]) {
+    } else if (currentTime > businessHours[i].id) {
         $("#work-hour_" + i).addClass("past");
     } else {
         $("#work-hour_" + i).addClass("future");
@@ -29,5 +72,16 @@ for (var i = 0; i < businessHours.length; i++) {
 
 // Save Button Listener
 
-$
+$(document).on('click', '.saveBtn', function () {
+    console.log(this.id);
+    var saveString = this.id;
+    var number = saveString[saveString.length - 1];
+    var textToSave = $("#timeSlot_" + number).val();
+    if (businessHours[number].task = "Available") {
+        console.log(textToSave);
+        localStorage.setItem("savedTask_" + number, textToSave);
+        var thingWeJustSet = localStorage.getItem("timeSlot_" + number);
+        console.log(thingWeJustSet);
+    }
+});
 
